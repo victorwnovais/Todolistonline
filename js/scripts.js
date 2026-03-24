3b43db0a538bbc501ad69d3a4fe3a70ce522ca10
Victor Novais <victorwnovais@hotmail.com>
2026-03-24 16:01:50 -0300

    functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
-alert("sctart JavaScript")
+const button = document.querySelector('.button-add-task')
+const input =document.querySelector('.input-task')
+
+let minhalistaDeItens =[]
+
+function adicionarNovaTarefa () {
+    minhalistaDeItens.push(input.value)
+
+    mostrarTarefas
+
+}
+
+function mostrarTarefas () {
+    
+    let novaLi 
+
+ //           <li class="task">
+ //               <img src="img/checked.png" alt="check">
+ //               <p>adicionado</p>
+ //               <img src="img/trash.png" alt="descarte">
+ //               <p>Feito!</p>
+ //           </li>
+}
+
+
+button.addEventListener('click',adicionarNovaTarefa )
